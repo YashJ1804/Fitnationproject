a85e9e7c3ef84e59d4d4ba8218c99457188c7c28
YashJ1804 <149168629+YashJ1804@users.noreply.github.com>
2025-12-14 18:44:28 +0530

    Refactor user schema to simplify fields

--- healthtrackernode/models/users.js
+++ healthtrackernode/models/users.js
@@ -1,15 +1,10 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
-    gender: { type: String, required: true },
-    password: { type: String, required: true }
-  },
-  { timestamps: true }
-);
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  points: { type: Number, default: 0 }
+});
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
